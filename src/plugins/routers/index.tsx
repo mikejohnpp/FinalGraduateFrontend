9a7a902fed545f76961262bc31adac17e662b461
mikejohnpp <tahoangphuc1901@gmail.com>
2026-04-29 23:13:16 +0700

    Added axios package

--- src/plugins/routers/index.tsx
+++ src/plugins/routers/index.tsx
@@ -1,31 +1,29 @@
 import Default from "@/views/layouts/Default";
 import React from "react";
-import {
-    createBrowserRouter,
-    type RouteObject,
-} from "react-router-dom";
+import { createBrowserRouter, type RouteObject } from "react-router-dom";
 
 export const PATH_CONSTRAINT = {
-    HOME: "/",
-    LOGIN: "/login",
-    // CHAT: "/chat",
-    // REGISTER: "/register",
+  HOME: "/",
+  LOGIN: "/login",
+  // CHAT: "/chat",
+  // REGISTER: "/register",
 };
 
 const Home = React.lazy(() => import("@/views/home/Home"));
 const Login = React.lazy(() => import("@/views/login/Login"));
 
 const routes: RouteObject[] = [
-    { path: PATH_CONSTRAINT.HOME, element: <Home /> },
-    { path: PATH_CONSTRAINT.LOGIN, element: <Login /> },
+  { path: PATH_CONSTRAINT.HOME, element: <Home /> },
+  { path: PATH_CONSTRAINT.LOGIN, element: <Login /> },
 ];
 
 const router = createBrowserRouter([
-    {
-        element: <Default />,
-        errorElement: <div>Đã có lỗi xảy ra</div>,
-        children: routes,
-    },
+  {
+    element: <Default />,
+    errorElement: <div>Đã có lỗi xảy ra</div>,
+    children: routes,
+  },
 ]);
 
 export default router;
+
