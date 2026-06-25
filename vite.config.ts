3c7c171516ab3af423fb880a6bfe7fcfa8fc4a94
mikejohnpp <tahoangphuc1901@gmail.com>
2026-06-25 19:09:18 +0700

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -32,5 +32,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.VITE_BASE_PATH || "/FinalGraduateFrontend",
+  base: process.env.VITE_BASE_PATH || "/",
 });
